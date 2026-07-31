a39cdb7ed1653581198e000876e81e225779f334
apetersonjr <apeterson@go4tpg.com>
2026-07-31 15:17:37 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
 import path from "node:path";
-
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath,
+  basePath: "/go4tpg",
   trailingSlash: true,
   turbopack: {
     root: path.join(__dirname),
   },
 };
-
 export default nextConfig;
